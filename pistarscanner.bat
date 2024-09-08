a7092ba52b4bf1ed086ab7ac63257241db1fa41e
techcow2 <108380065+techcow2@users.noreply.github.com>
2024-09-07 21:04:08 -0400

    Add files via upload

--- pistarscanner.bat
+++ pistarscanner.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Set code page to UTF-8 to support extended characters (e.g., ©)
 chcp 65001 >nul
 
 for /f "tokens=3" %%i in ('reg query HKEY_CURRENT_USER\Console ^| findstr /i "VirtualTerminalLevel"') do (
@@ -29,7 +28,7 @@ echo ======================================================%RESET%
 echo %CYAN%Would you like to proceed? [Y/N]%RESET%
 set /p user_choice=Enter your choice: 
 if /i "!user_choice!"=="Y" (
-    goto :continue
+    goto :choose_connection_type
 ) else if /i "!user_choice!"=="N" (
     echo %RED%Exiting the script. Goodbye!%RESET%
     goto :end
@@ -38,23 +37,121 @@ if /i "!user_choice!"=="Y" (
     goto :ask_to_proceed
 )
 
-:continue
+:choose_connection_type
 echo %CYAN%======================================================
-echo Welcome to the Pi-Star Network Scanner
-echo This script will scan your local Wi-Fi network and find
-echo the device running "http://pi-star/".
-echo Please wait while we search for the device...
+echo Is your Pi-Star connected via Ethernet or Wi-Fi?
+echo 1. Ethernet
+echo 2. Wi-Fi
+echo =======================================================
+set /p connection_type=Enter your choice (1 or 2): 
+
+if "!connection_type!"=="1" (
+    goto :ethernet_scan
+) else if "!connection_type!"=="2" (
+    goto :wifi_scan
+) else (
+    echo %RED%Invalid choice. Please enter 1 for Ethernet or 2 for Wi-Fi.%RESET%
+    goto :choose_connection_type
+)
+
+:ethernet_scan
+echo %CYAN%======================================================
+echo Scanning for active Ethernet connections...
 echo ======================================================%RESET%
 
-timeout /t 2 /nobreak >nul
+setlocal enabledelayedexpansion
+set /A count=0
+for /f "tokens=1-5 delims=:" %%a in ('ipconfig ^| findstr /R /C:"^Ethernet adapter" /C:"^ *IPv4 Address"') do (
+    if "%%a %%b %%c %%d %%e"=="Ethernet adapter" (
+        set /A count+=1
+        set adapter%%count%%=%%f
+        set "description[%%count%%]=%%f"
+    )
+    if "%%a"=="IPv4" (
+        set "ip[%%count%%]=%%c"
+    )
+)
+
+if !count!==0 (
+    echo %RED%No active Ethernet connections found.%RESET%
+    goto :end
+)
+
+echo %YELLOW%Available Ethernet connections:%RESET%
+for /L %%i in (1,1,!count!) do (
+    echo %%i. !description[%%i]! (!ip[%%i]!)
+)
+
+echo Press the number corresponding to the Ethernet network your Pi-Star is connected to or press [I] for "I don't know": 
+set /p ethernet_choice=Enter your choice: 
+
+if /i "!ethernet_choice!"=="I" (
+    echo %YELLOW%Scanning all Ethernet connections for Pi-Star...%RESET%
+    goto :scan_ethernet_networks
+)
+
+if "!ip[%ethernet_choice%]!"=="" (
+    echo %RED%Invalid choice. Please try again.%RESET%
+    goto :ethernet_scan
+)
+
+set selectedEthernetIP=!ip[%ethernet_choice%]!
+set baseEthernetIP=!selectedEthernetIP:~0,-1!.
+echo %YELLOW%Scanning the selected Ethernet network: !selectedEthernetIP!...%RESET%
+goto :scan_ethernet_specific
+
+:scan_ethernet_specific
+for /L %%i in (1,1,254) do (
+    set ip=!baseEthernetIP!%%i
+    echo Checking IP: !ip!
+    ping -n 1 -w 30 !ip! | find "TTL=" >nul
+    if !errorlevel! equ 0 (
+        curl -s --connect-timeout 0.3 -I http://!ip!/ | findstr "200 OK" >nul 2>&1
+        if !errorlevel! equ 0 (
+            curl -s --connect-timeout 0.3 http://!ip! | findstr /i "pi-star" >nul 2>&1
+            if !errorlevel! equ 0 (
+                echo %GREEN%Pi-Star found at IP address: !ip!%RESET%
+                set found=true
+                set piStarIP=!ip!
+                goto :open_browser
+            )
+        )
+    )
+)
+goto :not_found
+
+:scan_ethernet_networks
+for /L %%i in (1,1,!count!) do (
+    set baseIP=!ip[%%i]:~0,-1!.
+    for /L %%j in (1,1,254) do (
+        set ip=!baseIP!%%j
+        echo Checking IP: !ip!
+        ping -n 1 -w 30 !ip! | find "TTL=" >nul
+        if !errorlevel! equ 0 (
+            curl -s --connect-timeout 0.3 -I http://!ip!/ | findstr "200 OK" >nul 2>&1
+            if !errorlevel! equ 0 (
+                curl -s --connect-timeout 0.3 http://!ip! | findstr /i "pi-star" >nul 2>&1
+                if !errorlevel! equ 0 (
+                    echo %GREEN%Pi-Star found at IP address: !ip!%RESET%
+                    set found=true
+                    set piStarIP=!ip!
+                    goto :open_browser
+                )
+            )
+        )
+    )
+)
+goto :not_found
+
+:wifi_scan
+echo %CYAN%======================================================
+echo Scanning Wi-Fi network range for Pi-Star...
+echo ======================================================%RESET%
 
 for /f "tokens=3" %%i in ('netsh interface ip show addresses "Wi-Fi" ^| findstr "IP Address"') do set localIP=%%i
 
 if not defined localIP (
-    echo %RED%======================================================
-    echo Error: Could not find an active Wi-Fi connection.
-    echo Make sure your device is connected to a Wi-Fi network.
-    echo ======================================================%RESET%
+    echo %RED%Could not find an active Wi-Fi connection.%RESET%
     goto :end
 )
 
@@ -66,71 +163,30 @@ set found=false
 for /L %%i in (1,1,254) do (
     set ip=%baseIP%%%i
     echo Checking IP: !ip!
-    curl -s --connect-timeout 1 -I http://!ip!/ | findstr "200 OK" >nul 2>&1
+    ping -n 1 -w 30 !ip! | find "TTL=" >nul
     if !errorlevel! equ 0 (
-        curl -s --connect-timeout 1 http://!ip! | findstr /i "pi-star" >nul 2>&1
+        curl -s --connect-timeout 0.3 -I http://!ip!/ | findstr "200 OK" >nul 2>&1
         if !errorlevel! equ 0 (
-            echo %GREEN%======================================================
-            echo Pi-Star found at IP address: !ip!
-            echo ======================================================%RESET%
-            set found=true
-            set piStarIP=!ip!
-            goto :open_browser
+            curl -s --connect-timeout 0.3 http://!ip! | findstr /i "pi-star" >nul 2>&1
+            if !errorlevel! equ 0 (
+                echo %GREEN%Pi-Star found at IP address: !ip!%RESET%
+                set found=true
+                set piStarIP=!ip!
+                goto :open_browser
+            )
         )
     )
 )
 
+:not_found
+echo %RED%No Pi-Star device was found.%RESET%
+goto :end
+
 :open_browser
 if "!found!"=="true" (
-    echo %CYAN%======================================================
-    echo Opening the Pi-Star Dashboard in your default web browser...
-    echo ======================================================%RESET%
+    echo %CYAN%Opening the Pi-Star Dashboard in your default web browser...%RESET%
     start http://!piStarIP!
-    goto :end
 )
-
-if "!found!"=="false" (
-    echo %RED%======================================================
-    echo No Pi-Star device was found on the Wi-Fi network.
-    echo ======================================================%RESET%
-    echo Would you like to display all connected devices on the Wi-Fi network? [Y/N]
-    set /p response=Enter your choice: 
-
-    if /i "!response!"=="Y" (
-        goto :scan_all_ips
-    ) else if /i "!response!"=="Yes" (
-        goto :scan_all_ips
-    ) else (
-        echo %CYAN%======================================================
-        echo Thank you for using the Pi-Star Network Scanner.
-        echo ======================================================%RESET%
-        goto :end
-    )
-)
-
-:scan_all_ips
-echo %YELLOW%======================================================
-echo Scanning the Wi-Fi network for all connected devices...
-echo ======================================================%RESET%
-
-for /L %%i in (1,1,254) do (
-    set ip=%baseIP%%%i
-    ping -n 1 -w 50 !ip! | find "TTL=" >nul
-    if !errorlevel! equ 0 (
-        echo Device found at IP: !ip!
-    )
-)
-
-echo %CYAN%======================================================
-echo The devices above are connected to your Wi-Fi network.
-echo To locate Pi-Star, please follow these steps:
-echo =======================================================
-echo 1. Open your web browser (e.g., Chrome, Firefox, or Edge).
-echo 2. In the address bar at the top, type in one of the IP addresses.
-echo 3. Press Enter to load the page.
-echo 4. Look for the Pi-Star home page.
-echo 5. If it's not Pi-Star, try the next IP address.
-echo ======================================================%RESET%
 
 :end
 echo %CYAN%Press any key to exit...%RESET%
